95feb9c184cd1822d4d9ccd758deec4fead84453
sreekar-code <sreekar.writer@gmail.com>
2026-02-25 18:45:46 +0530

    Add new RSS feed URL to feeds array

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 const feeds = [
 
   "https://sreekarscribbles.com/feed.xml",
-  "https://aravindballa.com/rss.xml"
+  "https://aravindballa.com/rss.xml",
+  "https://rss.beehiiv.com/feeds/arqSjkRars.xml"
 ];
 
 const GOOGLE_FORM_URL = 'https://docs.google.com/forms/d/e/1FAIpQLSeW6icje8Lrm2LwJf51H5SpdufMQMcbSiUmRthoc8I8YQ20yg/viewform';
